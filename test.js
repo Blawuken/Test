0f08adca70444a26fbc87b298383255a5a515aca
Blawuken <jarbull87@gmail.com>
2023-10-15 00:42:19 +0700

    test jb4

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { Jb1, Jb2, Jb3, Jb4.} = require('./lib/canvas')
+const { Jb1, Jb2, Jb3, Jb4} = require('./lib/canvas')
 
 Jb4('https://i.ibb.co/DRr3mSw/danaqr.png', '@anggit0817', '- 081779122444 -', 'Hinata')
 .then(console.log)
