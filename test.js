7a976e1702ec50fc1a540894c91bcc9d7e3e133b
Blawuken <jarbull87@gmail.com>
2023-10-14 19:31:40 +0700

    Gabut

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { Jb1, Jb2, Jb3 } = require('./lib/canvas')
 
-Jb3('https://i.ibb.co/DRr3mSw/danaqr.png')
+Jb3('https://i.ibb.co/DRr3mSw/danaqr.png', 'Hinata Store')
 .then(console.log)
 .catch(console.error)
