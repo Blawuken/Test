f72c0bd432f0fe9dea064a2cc06672da0cf37a45
Blawuken <jarbull87@gmail.com>
2023-10-15 11:44:21 +0700

    text dowo

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { Jb1, Jb2, Jb3, Jb4} = require('./lib/canvas')
 
-Jb4('https://i.ibb.co/DRr3mSw/danaqr.png', '@anggit0817', '081779122444', 'Hinata')
+Jb4('https://i.ibb.co/DRr3mSw/danaqr.png', '@anggit081779122444', '081779122444', 'Hinata')
 .then(console.log)
 .catch(console.error)
