d23644cc97a6db2a32e89c9275b2a5e736785a74
Blawuken <jarbull87@gmail.com>
2023-10-15 14:06:21 +0700

    test canvas onepiece

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const { Jb1, Jb2, Jb3, Jb4} = require('./lib/canvas')
+const { Jb1, Jb2, Jb3, Jb4, OnePiece} = require('./lib/canvas')
 
-Jb4('https://i.ibb.co/DRr3mSw/danaqr.png', '@anggit081779122444', '081779122444', 'Hinata')
+OnePiece('HINATA', 'Store')
 .then(console.log)
 .catch(console.error)
