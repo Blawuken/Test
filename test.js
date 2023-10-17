409f465bb3b41aa1412d540fe6cc0794ecc85fc8
Blawuken <jarbull87@gmail.com>
2023-10-17 21:19:50 +0700

    Test jb6

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const { Jb1, Jb2, Jb3, Jb4, OnePiece} = require('./lib/canvas')
+const { Jb1, Jb2, Jb3, Jb4, OnePiece, Jb6} = require('./lib/canvas')
 
-OnePiece('HINATA', 'Store')
+Jb6('HINATA')
 .then(console.log)
 .catch(console.error)
